76379a82f33e45f886cc01fb9fd9b260f6e0d17f
Leon-guax <96957098+Leon-guax@users.noreply.github.com>
2022-12-06 18:03:09 +0100

    Day6

---
+++ 2022/Day6_code.py
@@ -0,0 +1,27 @@
+
+
+def all_unique(item):
+    #print(item, set(item))
+    return len(set(item)) == len(item)
+
+def find_N_different(inp_puzzle, N):
+    counter_1 =0
+    while ( counter_1 <= len(inp_puzzle)):
+        if all_unique(inp_puzzle[counter_1:counter_1+N]):
+            break
+        #print (inp_puzzle[counter_1:counter_1+N])
+        counter_1 +=1       
+    return counter_1 +N
+
+
+inp_puzzle = ''
+with open('Day6_input.txt') as f:
+    for line in f:
+
+        print(find_N_different(line, 4), 4)
+        print(find_N_different(line, 14), 14)
+
+   
+
+
+#%%
