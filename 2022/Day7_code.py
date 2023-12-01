0f840c14d6da6138782e5f065e3451b7991f1f84
Leon-guax <96957098+Leon-guax@users.noreply.github.com>
2023-12-01 15:49:43 +0100

    boh

--- 2022/Day7_code.py
+++ 2022/Day7_code.py
@@ -1,33 +1,22 @@
-class Tree(object):
 
-    def __init__(self, name, parent, size = -1):
-        self.name = name
-        self.children = []
-        self.size = size
-        self.parent = id(parent)
 
-    def createChild(self,name, size):
-            self.chilrend.append(Tree(name, size))
+def find_files(files, string):
+    subfolder = {}
+    for file in files:
+        if string in file.key():
+           subfolder[file.key()] = file.value()
+    return subfolder
     
-    def count_size(self):
-        for element in self.children:
-            if element.size<0:
-                element.count_size()
-            self.size += element.size
 
+files = {}
 
 
 
 with open('Day7_input.txt') as f:
     f.readline()
-
+    
     #for line in f:
      #   print(line)
       #  break
 
 #%%
-A=[1, 2, 3, [4, 5, 6]]
-print(id(A))
-A[0] = 'a'
-print(A)
-print(id(A))
